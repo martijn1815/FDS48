989aee8e5ae3faef625460f567434611c42fd01b
Martijn Schendstok <sch.martijn@outlook.com>
2020-09-24 11:45:35 +0200

    chash steps

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -5,6 +5,7 @@ Author:     FDS Group 48
 Date:       September 2020
 """
 
+import sys
 import argparse
 import os.path
 import pickle
@@ -15,6 +16,7 @@ import tarfile
 import json
 from geopy.geocoders import Nominatim
 from nltk.stem.wordnet import WordNetLemmatizer
+import gc
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -91,15 +93,15 @@ def clean_data(data_set):
     """
     stopword_list = stopwords.words('english')  # Define stopwords
 
-    data_set['text'] = data_set['text'].str.replace('@[^\s]+', '')
-    data_set['text'] = data_set['text'].str.replace('[^\w\s]', '')
-    data_set['text'] = data_set['text'].str.replace('\[.*?\]', '')
-    data_set['text'] = data_set['text'].str.replace('[‘’“”…]', '')
-    data_set['text'] = data_set['text'].str.replace('\w*\d\w*', '')
+    data_set['text_formatted'] = data_set['text'].str.replace('@[^\s]+', '')
+    data_set['text_formatted'] = data_set['text_formatted'].str.replace('[^\w\s]', '')
+    data_set['text_formatted'] = data_set['text_formatted'].str.replace('\[.*?\]', '')
+    data_set['text_formatted'] = data_set['text_formatted'].str.replace('[‘’“”…]', '')
+    data_set['text_formatted'] = data_set['text_formatted'].str.replace('\w*\d\w*', '')
 
-    data_set['text'] = data_set['text'].str.lower().str.split()  # lower and split the text
-    data_set['text'] = data_set['text'].apply(lambda x: [item for item in x if item not in stopword_list])  # remove stopwords
-    data_set['text'] = data_set['text'].apply(lemmatize_text)  # Lemmatize text
+    data_set['text_formatted'] = data_set['text_formatted'].str.lower().str.split()  # lower and split the text
+    data_set['text_formatted'] = data_set['text_formatted'].apply(lambda x: [item for item in x if item not in stopword_list])  # remove stopwords
+    data_set['text_formatted'] = data_set['text_formatted'].apply(lemmatize_text)  # Lemmatize text
 
     return data_set
 
@@ -163,7 +165,7 @@ def train(file_name):
 
         # Pre-Process Data
         print("Pre-Processing data:", end=" ")
-        data_set = data_set.sample(n = 1111111)  # Select random sample of size n (1,111,111 so training set is 1 million)
+        data_set = data_set.sample(n = 30000)  # Select random sample of size n (1,111,111 so training set is 1 million)
         data_set = clean_data(data_set)  # Data cleaning
 
         # Create data that puts tweet texts together with sentiment score
@@ -174,7 +176,7 @@ def train(file_name):
                     tweet_polarity = "negative"
                 else:
                     tweet_polarity = "positive"
-                tweets_and_polarity.append((data_set['text'].iloc[i], tweet_polarity))
+                tweets_and_polarity.append((data_set['text_formatted'].iloc[i], tweet_polarity))
         print("Done")
 
         # Feature extraction
@@ -188,7 +190,22 @@ def train(file_name):
         print("Done")
         print("Feature extraction:", end=" ")
 
-    final_data_set = [(extract_features(file_name, tweet), pol_score) for (tweet, pol_score) in tweets_and_polarity]
+    final_data_set = []
+    z = 100
+    #if not os.path.isfile("cashe_data_" + z + "_" + i + '.pickle'):
+
+    print(sys.maxsize)
+    for i in range(z):
+        x = int(len(tweets_and_polarity) / z * i)
+        y = int(len(tweets_and_polarity) / z * (i + 1))
+        data = tweets_and_polarity[x:y]
+        ds = [(extract_features(file_name, tweet), pol_score) for (tweet, pol_score) in data]
+        final_data_set += ds
+        del data
+        del ds
+        gc.collect()
+        print(i, sys.getsizeof(final_data_set))
+        #save_pickle_file(final_data_set, "cashe_data_" + z + "_" + i)
     print("Done")
 
     # Split training and test set
@@ -223,62 +240,99 @@ def classify_tweets(file_name, data_file):
     if not data_file:
         # Load Data
         print("Loading dataset:", end=" ")
-        tar = tarfile.open("geotagged_tweets_20160812-0912.tar")
-        files = tar.getmembers()
-        f = tar.extractfile(files[0])
-        #data = pd.read_json(f, lines=True)
-        # Efficient way of loading json to panda:
-        # https://medium.com/@ram.parameswaran22/a-relatively-faster-approach-for-reading-json-lines-file-into-pandas-dataframe-90b57353fd38
-        lines = f.read().splitlines()
-        df_inter = pd.DataFrame(lines[:100])
-        df_inter.columns = ['json_element']
-        df_inter['json_element'].apply(json.loads)
-        data = pd.json_normalize(df_inter['json_element'].apply(json.loads))
-        print("Done")
-        print(list(data))  # Column titles
+        if not os.path.isfile("cashe_data_tweet_1" + '.pickle'):
+            tar = tarfile.open("geotagged_tweets_20160812-0912.tar")
+            files = tar.getmembers()
+            f = tar.extractfile(files[0])
+            #data = pd.read_json(f, lines=True)
+            # Efficient way of loading json to panda:
+            # https://medium.com/@ram.parameswaran22/a-relatively-faster-approach-for-reading-json-lines-file-into-pandas-dataframe-90b57353fd38
+            lines = f.read().splitlines()
+            df_inter = pd.DataFrame(lines)
+            df_inter.columns = ['json_element']
+            df_inter['json_element'].apply(json.loads)
+            data = pd.json_normalize(df_inter['json_element'].apply(json.loads))
+            save_pickle_file(data, "cashe_data_tweet_1")
+        if not os.path.isfile("cashe_data_tweet_2" + '.pickle'):
+            if os.path.isfile("cashe_data_tweet_1" + '.pickle'):
+                data = load_pickle("cashe_data_tweet_1")
+                print("Done")
+            #print(list(data))  # Column titles
 
-        print("Cleaning dataset:", end=" ")
-        # Only included wanted columns
-        data = data[['id', 'text', 'created_at', 'source', 'lang',
-                     'place.country_code', 'place.bounding_box.coordinates', 'coordinates',
-                     'user.followers_count', 'user.friends_count']]
-        #print('\n', list(data))  # Column titles
+            print("Cleaning dataset:", end=" ")
+            # Only included wanted columns
+            data = data[['id', 'text', 'created_at', 'source', 'lang',
+                         'place.country_code', 'place.bounding_box.coordinates', 'coordinates',
+                         'user.followers_count', 'user.friends_count']]
+            save_pickle_file(data, "cashe_data_tweet_2")
+        if not os.path.isfile("cashe_data_tweet_3" + '.pickle'):
+            if os.path.isfile("cashe_data_tweet_2" + '.pickle'):
+                data = load_pickle("cashe_data_tweet_2")
+                print("Done")
+            print("Cleaning dataset:", end=" ")
+            #print('\n', list(data))  # Column titles
 
-        # Exclude tweets outside of US (location) - filter on country code US (Country where tweet is posted)
-        data = data[data['place.country_code'] == "US"]
-        data = data[data['lang'] == 'en']
+            # Exclude tweets outside of US (location) - filter on country code US (Country where tweet is posted)
+            data = data[data['place.country_code'] == "US"]
+            data = data[data['lang'] == 'en']
+
+            # Filter bots (hyperlinks in text)
+            # Removing dubious sources:
+            sources = ('<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>',
+                       '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+                       '<a href="http://twitter.com" rel="nofollow">Twitter Web Client</a>',
+                       '<a href="https://twitter.com/download/android" rel="nofollow">Twitter for Android Tablets</a>',
+                       '<a href="http://twitter.com/#!/download/ipad" rel="nofollow">Twitter for iPad</a>',
+                       '<a href="http://instagram.com" rel="nofollow">Instagram</a>')
+            data = data[data.source.isin(sources)]
+            # Removing people with no friends and no followers
+            data = data[(data['user.followers_count'] > 0) & (data['user.friends_count'] > 0)]
+            save_pickle_file(data, "cashe_data_tweet_3")
+
+        if not os.path.isfile("cashe_data_tweet_4" + '.pickle'):
+            if os.path.isfile("cashe_data_tweet_3" + '.pickle'):
+                data = load_pickle("cashe_data_tweet_3")
+                print("Done")
+                print(len(data))
+
+            # Determine tags and hashs (some tweets dont have tags only hash)
+            data['tags_mention'] = data.text.str.findall(r'(?<![@\w])@(\w{1,25})').apply(','.join)
+            data['hash_mention'] = data.text.str.findall(r"#(\w+)").apply(','.join)
+            data['tags_hash'] = data['hash_mention'] + data['tags_mention']
+            # Lower cases in tags and hashes
+            data['tags_hash'] = data['tags_hash'].apply(lambda x: x.lower())
+
+            save_pickle_file(data, "cashe_data_tweet_4")
+            print("test1")
+
+        if not os.path.isfile("cashe_data_tweet_5" + '.pickle'):
+            if os.path.isfile("cashe_data_tweet_4" + '.pickle'):
+                data = load_pickle("cashe_data_tweet_4")
+                print("Done")
+                print(len(data))
+
+            # See if either trum or clinton mentioned in hash or tags
+            data['Trump'] = data['tags_hash'].str.contains(r'trump|maga', na=False)
+            data['Clinton'] = data['tags_hash'].str.contains(r'clinton|imwithher|hillary', na=False)
+            data = data[(data['Trump'] != data['Clinton'])]
+
+            save_pickle_file(data, "cashe_data_tweet_5")
+
+        if os.path.isfile("cashe_data_tweet_5" + '.pickle'):
+            data = load_pickle("cashe_data_tweet_5")
+            print("Done")
+            print(len(data))
 
         # Get states
-        pd.set_option('display.max_columns', None)
+        data = data.sample(n=30000)
+        # pd.set_option('display.max_columns', None)
         data['latitude'] = data['place.bounding_box.coordinates'].apply(lambda x: x[0][0][0])
         data['longitude'] = data['place.bounding_box.coordinates'].apply(lambda x: x[0][0][1])
         data['coordinates'] = data['longitude'].astype(str) + ", " + data['latitude'].astype(str)
-        #print(data.head())
+        # print(data.head())
+        print("test2")
         data['state'] = data['coordinates'].apply(city_state_country)
-
-        # Determine tags and hashs (some tweets dont have tags only hash)
-        data['tags_mention'] = data.text.str.findall(r'(?<![@\w])@(\w{1,25})').apply(','.join)
-        data['hash_mention'] = data.text.str.findall(r"#(\w+)").apply(','.join)
-        data['tags_hash'] = data['hash_mention'] + data['tags_mention']
-        # Lower cases in tags and hashes
-        data['tags_hash'] = data['tags_hash'].apply(lambda x: x.lower())
-
-        # See if either trum or clinton mentioned in hash or tags
-        data['Trump'] = data['tags_hash'].str.contains(r'trump|maga', na=False)
-        data['Clinton'] = data['tags_hash'].str.contains(r'clinton|imwithher|hillary', na=False)
-        data = data[(data['Trump'] != data['Clinton'])]
-
-        # Filter bots (hyperlinks in text)
-        # Removing dubious sources:
-        sources = ('<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>',
-                   '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
-                   '<a href="http://twitter.com" rel="nofollow">Twitter Web Client</a>',
-                   '<a href="https://twitter.com/download/android" rel="nofollow">Twitter for Android Tablets</a>',
-                   '<a href="http://twitter.com/#!/download/ipad" rel="nofollow">Twitter for iPad</a>',
-                   '<a href="http://instagram.com" rel="nofollow">Instagram</a>')
-        data = data[data.source.isin(sources)]
-        # Removing people with no friends and no followers
-        data = data[(data['user.followers_count'] > 0) & (data['user.friends_count'] > 0)]
+        print("test3")
 
         save_pickle_file(data, file_name + "_twitter_data")
         print("Done")
@@ -297,7 +351,7 @@ def classify_tweets(file_name, data_file):
 
     # Apply classifier to column
     model = load_pickle(file_name)
-    data['classification_polarity'] = data['text'].apply(lambda x: model.classify((extract_features(file_name, x))))
+    data['classification_polarity'] = data['text_formatted'].apply(lambda x: model.classify((extract_features(file_name, x))))
     save_pickle_file(data, "twitter_data_classified")
     print("Done")
     #print("Columns", list(data))  # Column titles
