614c04b134c9fcdb17ad034271fb86f2795ff482
OlivierEverts <57379849+OlivierEverts@users.noreply.github.com>
2020-09-24 15:20:08 +0200

    R code for correlations and image files for graphs

---
+++ assignment1/correlation_code.R
@@ -0,0 +1,23 @@
+setwd("/Users/oliviereverts/Documents/GitHub/FDS48/assignment1")
+library(dplyr)
+library(tiddle)
+library(tidyr)
+
+# Read in the ipums proportions CSV file
+ipums_proportions <- read.csv("IPUMS_proportions.csv")
+
+# Read in the twitter sentiment proportions 
+twitter_proportions <- read.csv("sentiment_polarity_states_23k.csv")
+
+# Merge the two datasets
+merged_proportions <- merge(ipums_proportions, twitter_proportions, by.x = "State", by.y = "state")
+View(merged_proportions)
+
+# Make a subset with only the proportions 
+sub_merged_proportions <- select(merged_proportions, Proportion.Some.College, Proportion.No.College, 
+                                 Proportion.Not.In.Labor.Force, Proportion.Employed, Proportion.Unemployed,
+                                 trump_ratio, clinton_ratio, total_ratio)
+
+cor_mat <- cor(sub_merged_proportions)
+cor_mat <- round(cor_mat, 2)
+write.table(cor_mat, file="correlation_matrix.txt")
